d1aa10ea976bed36ddad5ae16254085f8c1a1577
aliozancilga <ali.ozan.cilga@gmail.com>
2021-09-09 18:46:38 +0300

    Initial commit

---
+++ MobiliumMovieAppCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobiliumMovieAppCase
+//
+//  Created by Ali Ozan CILGA on 9.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
